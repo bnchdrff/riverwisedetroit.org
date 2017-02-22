7a14544b5e2459fda9d0fd1438ab4c398892beff
Benjamin Chodoroff (bnchdrff) <anonyben@gmail.com>
2017-02-21 19:08:05 -0500

    header image

--- site/web/app/themes/riverwise_theme/templates/header.php
+++ site/web/app/themes/riverwise_theme/templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="/app/themes/riverwise_theme/assets/images/logo.png" alt="<?php bloginfo('name'); ?>" title="Go to homepage" /></a>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
